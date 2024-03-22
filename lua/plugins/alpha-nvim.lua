2d430648a256f7556634567675a6a5ca2eb01e63
Mingkai Hao <haomingkai@gmail.com>
2024-03-21 17:24:55 -0700

    chore: rewrite autocmd

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -17,6 +17,7 @@ return {
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
 
+    ------------------------- Dashboard settings -------------------------------
     -- Set header
     dashboard.section.header.val = {
       "                                                                       ",
@@ -59,12 +60,25 @@ return {
       dashboard.button("q", " " .. " Quit", "<cmd>qa!<cr>"),
     }
 
+    -- Set color
+    dashboard.section.header.opts.hl = "Keyword"               -- header
+    for _, button in ipairs(dashboard.section.buttons.val) do  -- buttons
+      button.opts.hl = "AlphaButtons"
+      button.opts.hl_shortcut = "AlphaShortcut"
+    end
+    dashboard.section.footer.opts.hl = "AlphaFooter"           -- footer
+    dashboard.opts.layout[1].val = 6
+
     -- Send config to alpha
     alpha.setup(dashboard.opts)
 
-    -- Set footer
-    -- dashboard.section.footer.val = fortune
-    vim.api.nvim_create_autocmd("User", {
+    ------------------------------ Autocmds ------------------------------------
+    local autocmd = vim.api.nvim_create_autocmd
+    local alphaaugroup = vim.api.nvim_create_augroup("Alpha", { clear = true })
+
+    autocmd("User", {
+      desc = "Set footer with loading time",
+      group = alphaaugroup,
       pattern = "LazyVimStarted",
       callback = function()
         local stats = require("lazy").stats()
@@ -81,18 +95,26 @@ return {
       end,
     })
 
-    -- Set color
-    dashboard.section.header.opts.hl = "Keyword"               -- header
-    for _, button in ipairs(dashboard.section.buttons.val) do  -- buttons
-      button.opts.hl = "AlphaButtons"
-      button.opts.hl_shortcut = "AlphaShortcut"
+    if vim.o.filetype == "lazy" then
+      vim.cmd.close()
+      autocmd("User", {
+        desc = "Load dashboard after Lazy installing plugins",
+        group = alphaaugroup,
+        pattern = "AlphaReady",
+        callback = function()
+          require("lazy").show()
+        end,
+      })
     end
-    dashboard.section.footer.opts.hl = "AlphaFooter"           -- footer
-    dashboard.opts.layout[1].val = 6
 
-    -- Disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-
+    autocmd("User", {
+      desc = "Disable folding on alpha buffer",
+      group = alphaaugroup,
+      pattern = "AlphaReady",
+      callback = function ()
+        vim.opt_local.foldenable = false
+      end
+    })
   end
   --                            ┏━━━━━━━━━━━━━┓
   --                            ┃ Config Ends ┃
