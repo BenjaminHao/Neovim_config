26e97034410ddc269bf77cd2b395ebb9d8d36da2
Mingkai Hao <haomingkai@gmail.com>
2024-04-01 13:57:09 -0700

    update: roll back lsp files

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,32 +1,23 @@
 --╭──────────────────────────────────────────────────────────────────────────╮--
 --│                                                                          │--
 --│ FILE: plugins/nvim-lspconfig.lua                                         │--
---│ DESC: LSP config file (all in one)                                       │--
+--│ DESC: LSP config file (npm is required)                                  │--
 --│                                                                          │--
 --╰──────────────────────────────────────────────────────────────────────────╯--
 -- TODO: Restructuring LSP files, add neodev.nvim
 return {
   "neovim/nvim-lspconfig",
-  event = { "BufReadPost", "BufNewFile" },
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = {
-    "williamboman/mason.nvim", -- package manager for LSP servers
-    "williamboman/mason-lspconfig.nvim", -- bridges mason with nvim-lspconfig
-    "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source
-    "folke/neodev.nvim" -- for nvim lua api
+    "williamboman/mason.nvim",
+    "hrsh7th/cmp-nvim-lsp",
   },
   --━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
   --                          ┃ Config Function ┃
   --                          ┗━━━━━━━━━━━━━━━━━┛
-  -- It's important that you set up the plugins in the following order:
-  -- 1. mason.nvim
-  -- 2. mason-lspconfig.nvim
-  -- 3. Setup servers via lspconfig
   config = function()
     local lspconfig = require("lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local map = require("core.utils").set_vim_keymap
     -- used to enable autocompletion (assign to every lsp server config)
     local default = cmp_nvim_lsp.default_capabilities()
     -- Change the Diagnostic symbols in the sign column (gutter)
@@ -37,37 +28,6 @@ return {
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 
-    ---------------------------- Mason Config ----------------------------------
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        border = "rounded",
-        icons = {
-          package_installed = "",
-          package_pending = "",
-          package_uninstalled = "",
-        },
-      },
-    })
-
-    ------------------------ Mason Lspconfig Config ----------------------------
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      -- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
-      ensure_installed = {
-        "clangd",    -- c/c++
-        "html",      -- HTML
-        "jdtls",     -- java
-        "jsonls",    -- json
-        "lua_ls",    -- lua
-        "pyright",   -- python
-        "tsserver",  -- javascript
-      },
-      -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
-
-    ------------------------ Nvim Lspconfig Config -----------------------------
     -- configure lua server (with special settings)
     lspconfig["lua_ls"].setup({
       capabilities = default,
@@ -96,10 +56,6 @@ return {
     lspconfig["jdtls"].setup({
       capabilities = default,
     })
-
-    ---------------------------- LSP Key Binds ---------------------------------
-    -- Mason
-    map("n", "<leader>L", "<cmd>Mason<cr>", { desc = "[L]SP Manager" })
 
   end,
   --                            ┏━━━━━━━━━━━━━┓
