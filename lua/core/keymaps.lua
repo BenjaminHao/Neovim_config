fed6dfa31ed78ebbef00e5dbe2556cea680bc28e
Mingkai Hao <haomingkai@gmail.com>
2024-04-03 12:31:30 -0700

    feat: add couple keybinds

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -40,22 +40,26 @@ map("i", "<C-a>", "<home>")
 map("i", "<C-e>", "<end>")
 map("i", "<C-f>", "<right>")
 map("i", "<C-b>", "<left>")
--- super colon/semicolon
+-- Super colon/semicolon
 map("i", "<C-,>", "<end>,")
 map("i", "<C-;>", "<end>;")
 -- input in next line
 map("i", "<C-cr>", "<end><cr>")
--- super tilde
+-- Super tilde
 map({ "n", "x" }, "~", function() util.invert_term() end)
 -- Clear search highlights
 map("n", "<esc>","<cmd>noh<cr><esc>")
 -- Better indenting
 map("x", "<", "<gv")
 map("x", ">", ">gv")
--- Visual overwrite paste
-map("x", "p", '"_dP')
+-- Smart indenting when insert in an empty line
+map("n", "i", function() return util.is_empty_line() and "S" or "i" end, { expr = true })
+-- Do not copy empty line when dd
+map("n", "dd", function() return util.is_empty_line() and '"_dd' or "dd" end, { expr = true })
 -- Do not copy on x. In Visual mode, d for cut, x for delete
 map({ "n", "x" }, "x", '"_x')
+-- Visual overwrite paste
+map("x", "p", '"_dP')
 -- Keep window centered when going up/down
 map("n", "J", "mzJ`z")
 map("n", "<C-d>", "<C-d>zz")
@@ -64,14 +68,15 @@ map("n", "<C-f>", "<C-f>zz")
 map("n", "<C-b>", "<C-b>zz")
 map("n", "N", "Nzzzv")
 map("n", "n", "nzzzv")
-
+-- Shortcut for quit and write
 map("n", "<leader>Q", "<cmd>qa!<cr>", { desc = "[Q]uit"})
-map("n", "<leader>w", "<cmd>w<cr>", { desc = "[w]rite"})
+map("n", "<leader>w", "<cmd>w!<cr>", { desc = "[w]rite File"})
+map("n", "<leader>W", "<cmd>wa!<cr>", { desc = "[W]rite All"})
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
 --                                ┃ BUFFERS ┃
 --                                ┗━━━━━━━━━┛
--- TODO: buffer key binds
 -- For other buffer key binds, see plugins/bufferline.lua
+-- TODO: Try alt key for buffers and windows
 map("n", "<leader>bn",  "<cmd>bn<CR>", { desc = "[n]ext Buffer"})
 map("n", "<leader>bp", "<cmd>bp<CR>", { desc = "[p]revious Buffer"})
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
@@ -90,13 +95,20 @@ map("n", "<C-up>", ":resize +2<CR>")
 map("n", "<C-down>", ":resize -2<CR>")
 map("n", "<C-right>", ":vertical resize +2<CR>")
 map("n", "<C-left>", ":vertical resize -2<CR>")
+--━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
+--                              ┃ DIAGNOSTICS ┃
+--                              ┗━━━━━━━━━━━━━┛
+map("n", "]d", vim.diagnostic.goto_next, { desc = "Next [d]iagnostic message" })
+map("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous [d]iagnostic message" })
+map("n", "<leader>de", vim.diagnostic.open_float, { desc = "Show diagnostic [e]rror messages" })
+map("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open diagnostic quickfix [l]ist" })
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
 --                                ┃ TOGGLES ┃
 --                                ┗━━━━━━━━━┛
-map("n", "<leader>Ts", function() toggle("spell", true, false, "Spell Check") end, { desc = "[s]pell Check"})
-map("n", "<leader>TL", function() toggle("number", true, false, "Line Number") end, { desc = "[L]ine Number"})
-map("n", "<leader>Tl", function() toggle("relativenumber", true, false, "Relative Line Number") end, { desc = "[l]ine Relative Number"})
-map("n", "<leader>Tr", function() util.toggle_set_color_column() end, { desc = "[r]uler Column" })
+map("n", "<leader>tl", function() toggle("relativenumber", true, false, "Relative Line Number") end, { desc = "[l]ine Relative Number"})
+map("n", "<leader>tL", function() toggle("number", true, false, "Line Number") end, { desc = "[L]ine Number"})
+map("n", "<leader>tr", function() util.toggle_set_color_column() end, { desc = "[r]uler Column" })
+map("n", "<leader>ts", function() toggle("spell", true, false, "Spell Check") end, { desc = "[s]pell Check"})
 --━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━--
 --                                ┃ PLUGINS ┃
 --                                ┗━━━━━━━━━┛
