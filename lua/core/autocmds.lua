6bbf139ced34cbb3a6e97bd5debfdcd99d26e1d9
Mingkai Hao <haomingkai@gmail.com>
2024-04-03 12:31:11 -0700

    feat: add relative number related autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -91,6 +91,29 @@ autocmd({ "VimResized" }, {
   end,
 })
 
+autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+  desc = "Enable relative number in normal mode",
+  group = general,
+  pattern = "*",
+  callback = function()
+    if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+
+autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
+  desc = "Disable relative number in insert mode",
+  group = general,
+  pattern = "*",
+  callback = function()
+    if vim.o.nu then
+      vim.opt.relativenumber = false
+      vim.cmd("redraw")
+    end
+  end,
+})
+
 -- autocmd("BufWinEnter", {
 --   desc = "Open help window in a vertical split to the left",
 --   group = general,
